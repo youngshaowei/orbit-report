963061e8edad257300c0f156db3c03ae320e49ef
Krystal Farwig <youngshaowei@gmail.com>
2019-10-27 22:06:44 -0500

    satellite data finally displayed

--- src/app/orbit-report/orbit-report.component.ts
+++ src/app/orbit-report/orbit-report.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Satellite } from '../satellite';
 
 @Component({
   selector: 'app-orbit-report',
   templateUrl: './orbit-report.component.html',
   styleUrls: ['./orbit-report.component.css']
 })
-export class OrbitReportComponent implements OnInit {
 
+export class OrbitReportComponent implements OnInit {
+  @Input() satellites: Satellite[];
   constructor() { }
 
   ngOnInit() {
